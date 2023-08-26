25b4a75f9d881e46dd354cff8624a312fea0ec72
Margus Liinev <margus.liinev@gmail.com>
2023-08-26 18:52:02 +0300

    testing swc builder again

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unsafe-call */
 import { IsEmail, IsNotEmpty, IsString, Length, MinLength, Matches, IsOptional } from 'class-validator';
 
 export class CreateUserDto {
