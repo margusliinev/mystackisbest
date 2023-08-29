6455d3115b450a30bbfb3d25db81a3f79a873f9b
Margus Liinev <margus.liinev@gmail.com>
2023-08-29 20:19:44 +0300

    shadcn setup with theme toggle

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,34 @@
-import { useState, useEffect } from 'react';
+import { Button, Input, Label } from './components/ui';
+import { ThemeToggle } from './components/theme-toggle';
 
 function App() {
-    const [greeting, setGreeting] = useState('');
-
-    useEffect(() => {
-        void fetch('/api')
-            .then((res) => res.text())
-            .then(setGreeting);
-    }, []);
-
     return (
-        <main>
-            <h1>{greeting}</h1>
-        </main>
+        <>
+            <nav className='absolute h-20 top-0 w-full grid'>
+                <div className='w-full max-w-6xl flex justify-between items-center mx-auto'>
+                    <h1 className='text-2xl font-semibold'>My Stack Is Best</h1>
+                    <ThemeToggle />
+                </div>
+            </nav>
+            <main className='w-screen h-screen grid place-items-center pb-20'>
+                <form className='w-full max-w-md bg-card shadow-md p-6 grid gap-6 rounded-md border'>
+                    <h1 className='text-xl font-semibold text-center'>Create your account</h1>
+                    <div className='grid gap-3'>
+                        <Label htmlFor='username'>Username</Label>
+                        <Input type='text' id='username' name='username'></Input>
+                    </div>
+                    <div className='grid gap-3'>
+                        <Label htmlFor='email'>Email</Label>
+                        <Input type='email' id='email' name='email'></Input>
+                    </div>
+                    <div className='grid gap-3'>
+                        <Label htmlFor='password'>Password</Label>
+                        <Input type='password' id='password' name='password'></Input>
+                    </div>
+                    <Button size={'sm'}>Sign up</Button>
+                </form>
+            </main>
+        </>
     );
 }
 
---
+++ client/src/components/ui/index.ts
@@ -0,0 +1,7 @@
+import { Button } from './button';
+import { Input } from './input';
+import { Label } from './label';
+import { Toast, ToastProvider, ToastViewport, ToastTitle, ToastDescription, ToastClose, ToastAction } from './toast';
+import { useToast, toast } from './use-toast';
+
+export { Button, Input, Label, Toast, ToastProvider, ToastViewport, ToastTitle, ToastDescription, ToastClose, ToastAction, useToast, toast };
